31709156208817ca5ffd6cbc0d5b0a26800d97aa
unknown <mpets18@gmail.com>
2026-01-29 11:33:54 -0300

    Trigger workflow

--- cypress/e2e/carritoDeCompras/carritoDeCompras.cy.js
+++ cypress/e2e/carritoDeCompras/carritoDeCompras.cy.js
@@ -47,7 +47,7 @@ describe('Carrito de Compras', () => {
     });
   });
 
-  it('Flujo de compra con usuario error', () => {
+  it.skip('Flujo de compra con usuario error', () => {
 
     loginPage
       .visitarPagina()
